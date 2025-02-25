e4564e6354573f068ef26741f73e9dedbbd9a3e5
VictorGabriel13 <victorgabrieldelimarocha@gmail.com>
2025-02-25 15:03:34 -0300

    Adição da API do Suap ao nosso sistema

---
+++ Server/settings.sample.js
@@ -0,0 +1,4 @@
+var CLIENT_ID = 'Od8bsSXPAjmpws4uIKseiekb0Kad5Sjk7ltwNCTM';
+var REDIRECT_URI = 'https://victorgabriel13.github.io/Calculadora-beta/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
