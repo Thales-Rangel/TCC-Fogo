dedd6177536ae4d6db264a37184066f4a93a2699
Thales-Rangel <thalesran2006@gmail.com>
2025-08-18 19:27:43 -0300

    Configurando método GET de dados dos registros da API

--- Server/server.js
+++ Server/server.js
@@ -13,3 +13,16 @@ const db = mysql.createConnection({
     database: 'tcc_fogo'
 });
 
+app.get('/dados', (req, res) => {
+    const query = 'SELECT * FROM registros';
+
+    db.query(query, (err, results) => {
+        if (err) throw err;
+
+        res.status(200).json(results);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
