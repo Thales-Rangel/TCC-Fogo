b175c9e6fe64323bda75b29374be9e871432af5f
Thales-Rangel <thalesran2006@gmail.com>
2025-08-18 18:56:04 -0300

    definições iniciais da API

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import mysql from 'mysql2';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'http://raspberrypi.local:3306',
+    user: 'root',
+    password: 'tcc-fogo',
+    database: 'tcc_fogo'
+});
+
