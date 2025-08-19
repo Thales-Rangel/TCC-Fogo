c0c638f3c86e5bd8ca7bec1506ed0a150719bb77
Thales-Rangel <thalesran2006@gmail.com>
2025-08-19 18:47:35 -0300

    Consertando senha do banco de servidor

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'admin',
+    password: 'tcc-fogo',
     database: 'tcc_fogo'
 });
 
